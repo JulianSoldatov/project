e59279252aeb964ee5caf0e8dd147017b3f7921c
Julian <test@gmail.com>
2021-07-13 16:07:23 +0700

    firts commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваше фамилия?', '');
+answers[2] = prompt('Как ваше вторая фамилия?', '');
+
+document.write(answers)
